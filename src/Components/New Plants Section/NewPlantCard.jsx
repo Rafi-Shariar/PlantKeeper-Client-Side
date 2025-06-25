f30049f11e9a09732b948c8b789babf644986e47
Rafi-Shariar <rafi.shariar619@gmail.com>
2025-06-25 22:50:46 +0600

    Fixed a bug

--- src/Components/New Plants Section/NewPlantCard.jsx
+++ src/Components/New Plants Section/NewPlantCard.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { FaExternalLinkAlt } from "react-icons/fa";
 import { Link } from "react-router";
 const NewPlantCard = ({ plant }) => {
-  const {_id, image, plantname, category, uploadData, carelevel } = plant;
+  const { _id, image, plantname, category, uploadData, carelevel } = plant;
 
   return (
     <div className=" bg-white rounded-xl shadow-md overflow-hidden hover:shadow-lg transition-shadow duration-300">
@@ -16,18 +16,22 @@ const NewPlantCard = ({ plant }) => {
       <div className="p-5 space-y-2">
         <h2 className="text-xl font-semibold text-green-700">{plantname}</h2>
 
-       <p className="text-sm text-gray-500 flex flex-col gap-2 md:gap-3 lg:flex-row justify-between border-b pb-5 border-green-600 dark:text-gray-300">
-  <span className="capitalize badge badge-md bg-green-100 text-green-800 dark:bg-green-600 dark:text-green-100">
-    <span className="font-semibold">CATEGORY:</span> {category}
-  </span> 
-  <span className="capitalize badge badge-md bg-green-100 text-green-800 mt-2 lg:mt-0 dark:bg-green-600 dark:text-green-100">
-    <span className="font-semibold">CARE LEVEL:</span> {carelevel}
-  </span> 
-</p>
+        <p className="text-sm text-gray-500 flex flex-col gap-2 md:gap-3 lg:flex-row justify-between border-b pb-5 border-green-600 dark:text-gray-300">
+          <span className="capitalize badge badge-md bg-green-100 text-green-800 dark:bg-green-600 dark:text-green-100">
+            <span className="font-semibold">CATEGORY:</span> {category}
+          </span>
+          <span className="capitalize badge badge-md bg-green-100 text-green-800 mt-2 lg:mt-0 dark:bg-green-600 dark:text-green-100">
+            <span className="font-semibold">CARE LEVEL:</span> {carelevel}
+          </span>
+        </p>
 
         <div className="flex items-center justify-between pt-3">
           <span className="text-xs text-gray-400">Uploaded: {uploadData}</span>
-          <Link to={`/details/${_id}`} className="btn  bg-green-500 hover:bg-green-700 text-white rounded-md px-4 py-1"><FaExternalLinkAlt />
+          <Link
+            to={`/details/${_id}`}
+            className="btn  bg-green-500 hover:bg-green-700 text-white rounded-md px-4 py-1"
+          >
+            <FaExternalLinkAlt />
             Explore
           </Link>
         </div>
